6ab3ec3aceb5090d7f1bb84b474200d4c7d53d8a
Marcell Biro <115219769+Marcel-zb96@users.noreply.github.com>
2024-02-27 13:38:51 +0100

    create security package with user service, jwtUtils, AuthTokenFilter, AuthEntryPointJwt and Websecconfig classes

---
+++ src/main/java/com/example/undercooked/security/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.example.undercooked.security;
+
+import com.example.undercooked.security.jwt.AuthEntryPointJwt;
+import com.example.undercooked.security.jwt.AuthTokenFilter;
+import com.example.undercooked.security.jwt.JwtUtils;
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    private UserDetailsService userDetailService;
+    private JwtUtils jwtUtils;
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    public WebSecurityConfig(UserDetailsService userDetailService, JwtUtils jwtUtils, AuthEntryPointJwt unauthorizedHandler) {
+        this.userDetailService = userDetailService;
+        this.jwtUtils = jwtUtils;
+        this.unauthorizedHandler = unauthorizedHandler;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/user/**").permitAll()
+                                .requestMatchers("/error").permitAll()
+                                .anyRequest().authenticated())
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+
+    }
+
+    public Filter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter(jwtUtils, userDetailService);
+    }
+
+    
+
+
+}
---
+++ src/main/java/com/example/undercooked/security/service/UserDetailServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.undercooked.security.service;
+
+import com.example.undercooked.model.Role;
+import com.example.undercooked.model.UserEntity;
+import com.example.undercooked.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.getUserEntityByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+        for (Role role : userEntity.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(role.name()));
+        }
+
+        return new User(userEntity.getName(), userEntity.getPassword(), roles);
+
+    }
+}
