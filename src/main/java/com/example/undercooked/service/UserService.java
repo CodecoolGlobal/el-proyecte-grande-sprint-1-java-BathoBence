5969e878d157e307e8a6e15d524f97195cfe072f
unknown <thebestsituation1996@gmail.com>
2024-03-13 17:37:45 +0100

    add new annotation to the IngredientMaterial field of the PantryItem so it can return in the response

--- src/main/java/com/example/undercooked/service/UserService.java
+++ src/main/java/com/example/undercooked/service/UserService.java
@@ -67,7 +67,6 @@ public class UserService {
 
     public List<RecipeInfoDTO> getAvailableRecipesBasedOnUserPantryItems(String userName){
         List<PantryItem> userPantryItems = getPantryItemsByUserName(userName);
-        System.out.println("*******************************");
         return recipeService.getRecipesByUserPantryItems(userPantryItems);
     }
 }
