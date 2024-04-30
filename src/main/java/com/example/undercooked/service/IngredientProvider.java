ab5f0c5dd53ee86f1cd0670ae10c08302690f621
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2024-04-30 14:15:26 +0200

    delete unused logger

--- src/main/java/com/example/undercooked/service/IngredientProvider.java
+++ src/main/java/com/example/undercooked/service/IngredientProvider.java
@@ -2,8 +2,6 @@ package com.example.undercooked.service;
 
 import com.example.undercooked.model.IngredientMaterial;
 import com.example.undercooked.repository.IngredientMaterialRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -12,7 +10,6 @@ import java.util.List;
 @Service
 public class IngredientProvider {
 
-    private static final Logger logger = LoggerFactory.getLogger(IngredientProvider.class);
     private final IngredientMaterialRepository ingredientMaterialRepository;
 
     public IngredientProvider(IngredientMaterialRepository ingredientMaterialRepository) {
